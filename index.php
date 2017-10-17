6e70f343079464d5338411c9c5d3e7f6b23e84bd
kevileem2 <32266918+kevileem2@users.noreply.github.com>
2017-10-17 10:00:34 +0200

    Added files for assignement 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $string1 = "Hello";
+    $string2 = "World";
+    echo "$string1 $string2"
+    ?>
+</body>
+</html>
